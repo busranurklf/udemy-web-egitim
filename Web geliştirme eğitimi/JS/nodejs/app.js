cbdfc3527ebdb479eaadb68be1ce9b51f4f19567
Büşranur <80097151+busranurklf@users.noreply.github.com>
2023-07-30 14:06:28 +0300

    Add files via upload

---
+++ Web geliştirme eğitimi/JS/nodejs/app.js
@@ -0,0 +1,4 @@
+const logger=require('./logger');
+
+logger.log('Hello');
+console.log(logger.name);
---
+++ Web geliştirme eğitimi/JS/nodejs/logger.js
@@ -0,0 +1,11 @@
+var url='http://logger.io/logger';
+var name='Büşra'
+
+function log(message) {
+    console.log(message);
+    
+}
+module.exports= {
+    name : name,
+    log:log
+};
